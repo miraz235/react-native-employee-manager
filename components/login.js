db3be4363f16175934cca5f21397ded204ac7efb
kawnayeen <kawnayeen.08@gmail.com>
2017-12-12 18:00:17 +0600

    card and card section developed

--- components/login.js
+++ components/login.js
@@ -1,15 +1,30 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import Card from './card';
+import CardSection from './card.section';
 
 export default class Login extends Component {
-  componentDidMount() {
-    this.props.titleBarApi("", "Please Login", "");
-  }
-  render() {
-    if (this.props.visible) {
-      return <Text>Login Page</Text>;
-    } else {
-      return <View />;
+    componentDidMount() {
+        this.props.titleBarApi("", "Please Login", "");
     }
-  }
+
+    render() {
+        if (this.props.visible) {
+            return (
+                <Card>
+                    <CardSection>
+                        <Text>Email</Text>
+                    </CardSection>
+                    <CardSection>
+                        <Text>Password</Text>
+                    </CardSection>
+                    <CardSection>
+                        <Text>Login Btn</Text>
+                    </CardSection>
+                </Card>
+            );
+        } else {
+            return <View/>;
+        }
+    }
 }
