6e456059dde764624e8ff7007cc7300d71deba72
kawnayeen <kawnayeen.08@gmail.com>
2017-12-12 17:13:18 +0600

    topbar modified

--- components/topbar.js
+++ components/topbar.js
@@ -1,24 +1,32 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class Topbar extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{this.props.leftText}</Text>
-        <Text>{this.props.title}</Text>
-        <Text>{this.props.rightText}</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{this.props.leftText}</Text>
+                <Text style={styles.textStyle}>{this.props.title}</Text>
+                <Text>{this.props.rightText}</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    marginTop: Platform.OS == "ios" ? 20 : 0,
-    justifyContent: "space-between",
-    padding: 10,
-    backgroundColor: "#e6e6e6"
-  }
+    container: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 });
