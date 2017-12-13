3fad2bda6c447477cc4a72198b740979b43f3001
kawnayeen <kawnayeen.08@gmail.com>
2017-12-13 17:07:09 +0600

    done with displaying list view

--- components/member-list.js
+++ components/member-list.js
@@ -1,16 +1,48 @@
 import React, {Component} from "react";
-import {Text, View} from "react-native";
+import {ListView, View} from "react-native";
+import MemberListItem from './member.list.item';
 
 export default class MemberList extends Component {
+
+    constructor() {
+        super();
+        this.employees = [
+            {'name': 'Himel'},
+            {'name': 'Rana'},
+            {'name': 'Shuvro'},
+            {'name': 'Proshad'}
+        ];
+    }
+
+    componentWillMount() {
+        this.createDataSource();
+    }
+
+    createDataSource(){
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.dataSource = ds.cloneWithRows(this.employees);
+    }
 
     componentDidMount() {
         if (this.props.visible)
             this.props.titleBarApi("", "Employees", "Add");
     }
 
+    renderRow(employee) {
+        return <MemberListItem employee={employee}/>;
+    }
+
     render() {
         if (this.props.visible) {
-            return <Text>Member List</Text>;
+            return (
+                <ListView
+                    enableEmptySections
+                    dataSource={this.dataSource}
+                    renderRow={this.renderRow}
+                />
+            );
         } else {
             return <View/>;
         }
